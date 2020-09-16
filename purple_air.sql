e50e0f36fd095feb3855ce8c4fee268d4030f88e
kellyhirano <hirano@cs.stanford.edu>
2020-09-16 01:04:03 -0700

    added pm25 and lrapa_aqi columns

--- purple_air.sql
+++ purple_air.sql
@@ -2,5 +2,7 @@ CREATE TABLE purple_air(
   datetime	TEXT	NOT NULL,
   id	TEXT	NOT NULL,
   aqi	INTEGER	NOT NULL,
+  lrapa_aqi INTEGER NOT NULL,
+  pm25 REAL NOT NULL,
   PRIMARY KEY (datetime, id)
 );
